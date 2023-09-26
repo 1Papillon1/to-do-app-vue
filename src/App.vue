dbbff544d8426ec23c290909297bc4c44b7a2e02
1Papillon1 <83593078+1Papillon1@users.noreply.github.com>
2023-09-26 21:28:26 +0200

    Pushing project

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <AppTitle />
+  </div>
+</template>
+
+<script>
+import AppTitle from './components/AppTitle.vue'
+
+export default {
+  name: 'App',
+  components: {
+    AppTitle,
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/AppTitle.vue
@@ -0,0 +1,25 @@
+<!-- Making list image: <a href="http://www.freepik.com">Designed by rawpixel.com / Freepik</a> -->
+<template>
+  <div class="hello">
+    <h1>Welcome to To Do App</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AppTitle',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.hello {
+  width: 100vw;
+  background-color: rgb(214, 214, 214);
+}
+
+
+</style>
