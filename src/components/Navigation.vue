fe8b8b2686ec4e74c19d34ba620dd370af80f0f8
1Papillon1 <83593078+1Papillon1@users.noreply.github.com>
2023-09-27 23:36:06 +0200

    Navigation component
    Pushing navigation component.

---
+++ src/components/Navigation.vue
@@ -0,0 +1,21 @@
+<template>
+  <nav class="navigation">
+    
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'Navigation',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+
+
+</style>
